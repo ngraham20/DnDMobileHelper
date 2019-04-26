29fb928932d1ec9d66f5c8ff86410c00235d0a08
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-04-26 00:10:31 -0700

    Initial Flutter Project Added
    Also, moved android studio project to sub folder

---
+++ flutter_project/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DnDMobileHelper());
+
+class DnDMobileHelper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
